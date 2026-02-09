5ea03394e41653aa0ba24b9d58f297c4d2313055
youssef abdmouleh <youssef.abdmouleh164@gmail.com>
2026-02-09 19:44:01 +0100

    .env

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleContollerWithType;
+use Symfony\Component\Routing\Loader\Configurator\Routes;
+use App\Models\Product;
 
 Route::get('/', function () {
     return view('welcome');
+use App\Models\Product;
 });
 Route::get('/home', function () {
     return view('home', ['name' => 'Youssef']);
@@ -15,4 +18,7 @@ Route::get('/test-layout', function () {
 Route::get('/exercice', function () {
     return view('mainPage');
 });
+Routes::get('/products/{id}', function ($id) {
+    $products = Product::find($id);
+})
 Route::resource('articles', ArticleContollerWithType::class);
